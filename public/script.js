ca87799054d0ee04f8d7198688f48288f7cac106
unknown <easytobeh@gmail.com>
2019-10-24 01:28:37 +0100

    Completed Users and admin sign in

---
+++ public/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+    //*************USERS SIGN IN**********************
+   
+    $("#login").on("click", function (event) {
+        event.preventDefault();
+
+        let listArray = [];
+        let user_name = $("#user").val();
+        let pass = $("#password").val();
+        user_name = user_name.toLowerCase();
+        var foundUser = false;
+
+
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/users",
+            cache: false,
+            dataType: 'json',
+            success: function (data) {
+              
+                $.each(data, function (i, UserObj) {
+                    if (UserObj.username == user_name) //user exists
+                    {
+                        if (UserObj.password == pass) {
+                            //listArray = UserObj.list;
+                            sessionStorage.setItem('index', i);
+                            sessionStorage.setItem('currentUser', user_name);
+                            foundUser = true;   
+                        }
+                        else {
+                            alert("Username or password incorrect");
+                        }
+                    }
+                    else { //user doesn't exist
+                        return;
+                    }
+                });
+
+                if (foundUser) {
+                   window.location.href = "UserHouseList.html";    
+                }
+                foundUser = false;
+                user_name = "";
+                pass = "";
+            },
+            error: function () {
+                alert("Error fetching data");
+            }
+        });
+    });/**********************End user Signin******************************** */
+
+  
+
+});
+
+
+
+
+
+
